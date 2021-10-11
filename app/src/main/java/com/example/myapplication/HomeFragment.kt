83363d273ada4942a87a24fb1c95ab93c51644ae
moanrisy <mantondev@gmail.com>
2021-10-11 09:12:21 +0700

    Add fragment manager

--- app/src/main/java/com/example/myapplication/HomeFragment.kt
+++ app/src/main/java/com/example/myapplication/HomeFragment.kt
@@ -5,6 +5,7 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.example.myapplication.databinding.ActivityMainBinding
 
 // TODO: Rename parameter arguments, choose names that match
 // This is home fragment
@@ -21,6 +22,7 @@ class HomeFragment : Fragment() {
     // TODO: Rename and change types of parameters
     private var param1: String? = null
     private var param2: String? = null
+    private lateinit var binding: ActivityMainBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -2,33 +2,57 @@ package com.example.myapplication
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.fragment.app.FragmentManager
+import androidx.fragment.app.FragmentTransaction
 import com.example.myapplication.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
 
+    inline fun FragmentManager.inTransaction(func: FragmentTransaction.()
+    -> Unit) {
+        val fragmentTransaction = beginTransaction()
+        fragmentTransaction.func()
+        fragmentTransaction.commit()
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 //        setContentView(R.layout.activity_main)
+        supportFragmentManager.inTransaction {
+            add(R.id.frameLayout, HomeFragment())
+        }
 
         binding.bottomNav.setOnItemSelectedListener {
             when (it.itemId) {
                 R.id.homeFragment -> {
+                    supportFragmentManager.inTransaction {
+                        replace(R.id.frameLayout, HomeFragment())
+                    }
                     setContent("Home")
                     true
                 }
                 R.id.forumFragment -> {
+                    supportFragmentManager.inTransaction {
+                        replace(R.id.frameLayout, ForumFragment())
+                    }
                     setContent("Forum")
                     true
                 }
                 R.id.settingFragment -> {
+                    supportFragmentManager.inTransaction {
+                        replace(R.id.frameLayout, SettingFragment())
+                    }
                     setContent("Search")
                     true
                 }
                 R.id.profileFragment -> {
+                    supportFragmentManager.inTransaction {
+                        replace(R.id.frameLayout, ProfileFragment())
+                    }
                     setContent("Profile")
                     true
                 }
